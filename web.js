64e31f04b298a389d12fa19a97eeb203fdab1830
Michael Montesines <mikezian@yahoo.com>
2013-07-10 20:31:48 +0000

    Read contents on index.html and send it as response

--- web.js
+++ web.js
@@ -1,14 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    //response.send('Hello World 2!');
-    var contents = fs.readfileSync('index.html');
-    var buffer = new Buffer(contents, 'utf-8');
-    response.send(buffer.toString('utf-8');
+  var contents = fs.readfileSync('index.html');
+  var buffer = new Buffer(contents, 'utf-8');
+  response.send(buffer.toString('utf-8')); 
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
