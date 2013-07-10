0843d66446f8394ebda9962142595e9f4b863521
Michael Montesines <mikezian@yahoo.com>
2013-07-10 20:33:21 +0000

    Misspelled function

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var contents = fs.readfileSync('index.html');
+  var contents = fs.readFileSync('index.html');
   var buffer = new Buffer(contents, 'utf-8');
   response.send(buffer.toString('utf-8')); 
 });
